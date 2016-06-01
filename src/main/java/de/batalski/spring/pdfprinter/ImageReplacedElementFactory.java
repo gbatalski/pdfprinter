d5def2d80c2866c56d64d89e11b86a77fb9499a9
Ghenadii Batalski <Ghenadii.Batalski@gmail.com>
2016-06-01 23:36:32 +0200

    some warnings removed

--- src/main/java/de/batalski/spring/pdfprinter/ImageReplacedElementFactory.java
+++ src/main/java/de/batalski/spring/pdfprinter/ImageReplacedElementFactory.java
@@ -1,8 +1,6 @@
 package de.batalski.spring.pdfprinter;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
